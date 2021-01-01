e16dbaebf2e40ebb7ad4cc1fdf3960da9bb30e4c
d4t4guy <andre6180@gmail.com>
2021-01-01 09:21:06 -0700

    updating main script

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,28 @@
+#testing
 print("hello world!")
 print("goodbye world!")
+
+# Requirements
+# Input = budget_data.csv in resources folder
+# Outputs:
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+#psuedo-code:
+""" Variables: 
+OUTPUTS
+total_months as integer 
+Net_total (over P&L column) as float
+Avg_Delta as float 
+Max_increase as float
+Max_decrease as float --will probably make this min_increase
+Best_Month, Worst_Month as date
+
+INPUTS
+Months[]
+
+PROCESS
+ """
