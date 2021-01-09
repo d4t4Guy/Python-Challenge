73b8bf8313da6496daa4e4d2ca1efd4e0b6e1851
d4t4guy <andre6180@gmail.com>
2021-01-09 15:17:21 -0700

    i dunno

---
+++ PyPoll/main.py
@@ -0,0 +1,6 @@
+# pyPoll - counts votes and returns summary
+# input: CSV file containing voter id, county, candidate 
+# steps
+#   Import id's into list; import votes into another list
+#   Combine into dict with ID as key and candidate as value
+#   
